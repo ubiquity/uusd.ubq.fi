3344acd675c73fb85261661d9df08d905ad7eb65
zugdev <zug.dev@proton.me>
2024-10-12 17:07:06 -0300

    feat: export some shared constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+import { SupportedChainId } from "@cowprotocol/cow-sdk";
+export const backendAddress = '0xbB689fDAbBfc0ae9102863E011D3f897b079c80F';
+export const mainnet = SupportedChainId.MAINNET;
