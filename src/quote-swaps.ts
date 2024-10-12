62307a3327b08d911481e0ce456da4f3107e581b
zugdev <zug.dev@proton.me>
2024-10-12 17:37:32 -0300

    feat: backend address is the one to execute swaps

--- src/quote-swaps.ts
+++ src/quote-swaps.ts
@@ -1,7 +1,7 @@
 import { OrderBookApi, OrderQuoteSideKindSell } from "@cowprotocol/cow-sdk";
 import { Token } from "./fetch-tokens";
 import { utils } from "ethers";
-import { mainnet } from "./constants";
+import { backendAddress, mainnet } from "./constants";
 import { appState } from "./main";
 
 const LUSD_ADDRESS = "0x5f98805a4e8be255a32880fdec7f6728c6568ba0";
@@ -9,6 +9,7 @@ const UBQ_ADDRESS = "0x4e38d89362f7e5db0096ce44ebd021c3962aa9a0";
 
 /**
  * This function will return quotes necessary to swap into a 95% LUSD and 5% UBQ split. 
+ * It will be called with our backend address and not the user's.
  * It returns either a quote object or null if the swap is not necessary.
  * @param input The input token object
  * @param inputAmount The amount of input token without decimals
@@ -16,24 +17,22 @@ const UBQ_ADDRESS = "0x4e38d89362f7e5db0096ce44ebd021c3962aa9a0";
  */
 export async function quoteSwaps(input: Token, inputAmount: number) {
   const isConnected = appState.getIsConnectedState();
-  const address = appState.getAddress();
   const selectedChainId = appState.getChainId();
 
   const orderBookApi = new OrderBookApi({ chainId: mainnet });
 
-  if (!isConnected || !address) throw Error("User not connected");
+  if (!isConnected || !backendAddress) throw Error("User not connected");
   if (selectedChainId !== mainnet) throw Error("Invalid network");
 
   let quoteLusd = null;
   let quoteUbq = null;
 
-  console.log(input.address);
   if (input.address !== LUSD_ADDRESS) {
     const quoteRequestLusd = {
       sellToken: input.address,
       buyToken: LUSD_ADDRESS,
-      from: address,
-      receiver: address,
+      from: backendAddress,
+      receiver: backendAddress,
       sellAmountBeforeFee: utils.parseUnits((0.95 * inputAmount).toString(), input.decimals).toString(),
       kind: OrderQuoteSideKindSell.SELL,
     };
@@ -46,8 +45,8 @@ export async function quoteSwaps(input: Token, inputAmount: number) {
     const quoteRequestUbq = {
       sellToken: input.address,
       buyToken: UBQ_ADDRESS,
-      from: address,
-      receiver: address,
+      from: backendAddress,
+      receiver: backendAddress,
       sellAmountBeforeFee: utils.parseUnits((0.05 * inputAmount).toString(), input.decimals).toString(),
       kind: OrderQuoteSideKindSell.SELL,
     };
