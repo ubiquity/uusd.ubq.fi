f902b63efb76f7343301cdb252ad1e085cb9e060
zugdev <zug.dev@proton.me>
2024-10-12 17:06:31 -0300

    feat: manually handle provider since wallet connect is down for now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createAppKit } from "@reown/appkit";
 import { EthersAdapter } from "@reown/appkit-adapter-ethers";
-import { mainnet, gnosis } from "@reown/appkit/networks";
+import { mainnet } from "@reown/appkit/networks";
 import { renderHeader } from "./render/render-header";
-import { fetchTokens, Token } from "./fetch-tokens";
+import { fetchTokens } from "./fetch-tokens";
 import { quoteSwaps } from "./quote-swaps";
+import { executeSwaps } from "./swap";
+import { ethers } from "ethers";
 
 const projectId = "415760038f8e330de4868120be3205b8";
 
@@ -14,9 +16,14 @@ const metadata = {
   icons: ["https://avatars.githubusercontent.com/u/76412717"],
 };
 
-const modal = createAppKit({
+// create a default provider & signer for Ethereum mainnet
+export const provider = new ethers.providers.JsonRpcProvider("https://eth.llamarpc.com");
+export const signer = provider.getSigner();
+
+export const appState = createAppKit({
   adapters: [new EthersAdapter()],
-  networks: [mainnet, gnosis],
+  networks: [mainnet],
+  defaultNetwork: mainnet,
   metadata,
   projectId,
   features: {
@@ -24,15 +31,40 @@ const modal = createAppKit({
   },
 });
 
+async function waitForConnection() {
+  return new Promise<void>((resolve) => {
+    const interval = setInterval(() => {
+      if (appState.getIsConnectedState()) {
+        console.log(`User connected: ${appState.getAddress()}`);
+        clearInterval(interval);
+        resolve();
+      } else {
+        console.log("Waiting for user to connect...");
+      }
+    }, 1000); // poll every second
+  });
+}
+
 export async function mainModule() {
   try {
+    console.log("Provider: ", provider);
+    console.log("Signer: ", signer);
+
     console.log("Initializing Reown AppKit...");
     renderHeader();
+
+    console.log("Waiting for user connection...");
+    await waitForConnection(); // wait until the user connects
+
     console.log("Fetching tokens...");
     const tokens = await fetchTokens();
-    console.log("tokens: ", tokens);
-    console.log("Quoting swaps: ");
-    await quoteSwaps(tokens[0], 1000000);
+    console.log("Tokens: ", tokens);
+
+    console.log("Quoting swaps...");
+    const { quoteLusd, quoteUbq } = await quoteSwaps(tokens[0], 1000000);
+
+    console.log("Executing swaps...");
+    executeSwaps(quoteLusd, quoteUbq);
   } catch (error) {
     console.error("Error in main:", error);
   }
---
+++ src/swap.ts
@@ -0,0 +1,8 @@
+import { backendAddress, mainnet } from './constants';
+import { OrderParameters } from '@cowprotocol/cow-sdk';
+import { useAppKitProvider } from '@reown/appkit/react';
+import { appState } from './main';
+
+export async function executeSwaps(quoteLusd : OrderParameters|null, quoteUbq : OrderParameters|null) {
+    
+}
