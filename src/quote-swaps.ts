4e6d6b002a4d18af854bfce2d4e0208a0ae67ca5
zugdev <zug.dev@proton.me>
2024-10-12 17:18:19 -0300

    feat: handle case where input token is either LUSD or UBQ

--- src/quote-swaps.ts
+++ src/quote-swaps.ts
@@ -4,6 +4,16 @@ import { utils } from "ethers";
 import { mainnet } from "./constants";
 import { appState } from "./main";
 
+const LUSD_ADDRESS = "0x5f98805a4e8be255a32880fdec7f6728c6568ba0";
+const UBQ_ADDRESS = "0x4e38d89362f7e5db0096ce44ebd021c3962aa9a0";
+
+/**
+ * This function will return quotes necessary to swap into a 95% LUSD and 5% UBQ split. 
+ * It returns either a quote object or null if the swap is not necessary.
+ * @param input The input token object
+ * @param inputAmount The amount of input token without decimals
+ * @returns Either a quote object or null if the swap is not necessary
+ */
 export async function quoteSwaps(input: Token, inputAmount: number) {
   const isConnected = appState.getIsConnectedState();
   const address = appState.getAddress();
@@ -14,31 +24,38 @@ export async function quoteSwaps(input: Token, inputAmount: number) {
   if (!isConnected || !address) throw Error("User not connected");
   if (selectedChainId !== mainnet) throw Error("Invalid network");
 
-  // 95% of input amount to LUSD
-  let quoteRequest = {
-    sellToken: input.address,
-    buyToken: "0x5f98805A4E8be255a32880FDeC7F6728C6568bA0", // LUSD
-    from: address,
-    receiver: address,
-    sellAmountBeforeFee: utils.parseUnits((0.95 * inputAmount).toString(), input.decimals).toString(),
-    kind: OrderQuoteSideKindSell.SELL,
-  };
+  let quoteLusd = null;
+  let quoteUbq = null;
 
-  const { quote: quoteLusd } = await orderBookApi.getQuote(quoteRequest);
-  console.log("LUSD:", quoteLusd);
+  console.log(input.address);
+  if (input.address !== LUSD_ADDRESS) {
+    const quoteRequestLusd = {
+      sellToken: input.address,
+      buyToken: LUSD_ADDRESS,
+      from: address,
+      receiver: address,
+      sellAmountBeforeFee: utils.parseUnits((0.95 * inputAmount).toString(), input.decimals).toString(),
+      kind: OrderQuoteSideKindSell.SELL,
+    };
 
-  // 5% of input amount to UBQ
-  quoteRequest = {
-    sellToken: input.address,
-    buyToken: "0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0", // UBQ
-    from: address,
-    receiver: address,
-    sellAmountBeforeFee: utils.parseUnits((0.05 * inputAmount).toString(), input.decimals).toString(),
-    kind: OrderQuoteSideKindSell.SELL,
-  };
+    quoteLusd = (await orderBookApi.getQuote(quoteRequestLusd)).quote;
+    console.log("LUSD Quote:", quoteLusd);
+  }
 
-  const { quote: quoteUbq } = await orderBookApi.getQuote(quoteRequest);
-  console.log("UBQ:", quoteUbq);
+  if (input.address !== UBQ_ADDRESS) {
+    const quoteRequestUbq = {
+      sellToken: input.address,
+      buyToken: UBQ_ADDRESS,
+      from: address,
+      receiver: address,
+      sellAmountBeforeFee: utils.parseUnits((0.05 * inputAmount).toString(), input.decimals).toString(),
+      kind: OrderQuoteSideKindSell.SELL,
+    };
 
+    quoteUbq = (await orderBookApi.getQuote(quoteRequestUbq)).quote;
+    console.log("UBQ Quote:", quoteUbq);
+  }
+
+  // If the input token is LUSD or UBQ that quote will be null and therefore that swap won't happen which means no fees
   return { quoteLusd, quoteUbq };
 }
