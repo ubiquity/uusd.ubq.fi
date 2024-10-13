890d8b0e9c9c70e37c0d5b39244a7af9d8735f9c
zugdev <zug.dev@proton.me>
2024-10-12 22:08:34 -0300

    feat: add executeSwaps

--- src/swap.ts
+++ src/swap.ts
@@ -1,6 +1,32 @@
 import { backendAddress, mainnet } from "./constants";
-import { OrderParameters } from "@cowprotocol/cow-sdk";
-import { useAppKitProvider } from "@reown/appkit/react";
-import { appState } from "./main";
+import { OrderBookApi, OrderParameters, OrderSigningUtils, SigningScheme, UnsignedOrder } from "@cowprotocol/cow-sdk";
+import { backendSigner } from "./main";
 
-export async function executeSwaps(quoteLusd: OrderParameters | null, quoteUbq: OrderParameters | null) {}
+export async function executeSwaps(quoteLusd: OrderParameters | null, quoteUbq: OrderParameters | null) {
+
+    const orderBookApi = new OrderBookApi({ chainId: mainnet });
+
+    if(quoteLusd){
+        const lusdOrder: UnsignedOrder = {
+            ...quoteLusd,
+            receiver: backendAddress,
+            validTo: Math.round((Date.now() + 200_000) / 1000),
+            partiallyFillable: false,
+        };
+
+        const lusdSignedOrder = await OrderSigningUtils.signOrder(lusdOrder, mainnet, backendSigner);
+        const lusdOrderId = await orderBookApi.sendOrder({ ...lusdOrder, signingScheme: lusdSignedOrder.signingScheme as string as SigningScheme, signature: lusdSignedOrder.signature});
+    }
+    
+    if(quoteUbq){
+        const ubqOrder: UnsignedOrder = {
+            ...quoteUbq,
+            receiver: backendAddress,
+            validTo: Math.round((Date.now() + 200_000) / 1000),
+            partiallyFillable: false,
+        };
+
+        const ubqSignedOrder = await OrderSigningUtils.signOrder(ubqOrder, mainnet, backendSigner)
+        const ubqOrderId = await orderBookApi.sendOrder({ ...ubqOrder, signingScheme: ubqSignedOrder.signingScheme as string as SigningScheme, signature: ubqSignedOrder.signature});
+    }
+}
