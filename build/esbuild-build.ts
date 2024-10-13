4c9fbdd4dadbd42b590b9f552b97b9fd42fa4c27
zugdev <zug.dev@proton.me>
2024-10-12 21:14:52 -0300

    chore: manage package and build script

--- build/esbuild-build.ts
+++ build/esbuild-build.ts
@@ -1,9 +1,12 @@
+import { config } from "dotenv";
 import esbuild from "esbuild";
+config();
+
 const typescriptEntries = ["src/main.ts"];
-// const cssEntries = ["static/style.css"];
+const cssEntries = ["static/style.css"];
 const entries = [
   ...typescriptEntries,
-  //  ...cssEntries
+  ...cssEntries
 ];
 
 export const esBuildContext: esbuild.BuildOptions = {
@@ -18,16 +21,34 @@ export const esBuildContext: esbuild.BuildOptions = {
     ".eot": "dataurl",
     ".ttf": "dataurl",
     ".svg": "dataurl",
+    ".json": "dataurl",
   },
   outdir: "static/dist",
+  define: createEnvDefines(["BACKEND_PRIVATE_KEY"], {
+    BACKEND_PRIVATE_KEY: process.env.BACKEND_PRIVATE_KEY
+  }),
 };
 
 esbuild
   .build(esBuildContext)
-  .then(() => {
-    console.log("\tesbuild complete");
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  .then(() => {console.log("\tesbuild complete");})
+  .catch((err) => {console.error(err);});
+
+  function createEnvDefines(environmentVariables: string[], generatedAtBuild: Record<string, unknown>): Record<string, string> {
+    const defines: Record<string, string> = {};
+    for (const name of environmentVariables) {
+      const envVar = process.env[name];
+      if (envVar !== undefined) {
+        defines[name] = JSON.stringify(envVar);
+      } else {
+        console.log(process.env.BACKEND_PRIVATE_KEY);
+        throw new Error(`Missing environment variable: ${name}`);
+      }
+    }
+    for (const key in generatedAtBuild) {
+      if (Object.prototype.hasOwnProperty.call(generatedAtBuild, key)) {
+        defines[key] = JSON.stringify(generatedAtBuild[key]);
+      }
+    }
+    return defines;
+  }
