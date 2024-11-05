f107bfa1fb24264b109396c8043da781d3637178
zugdev <zug.dev@proton.me>
2024-11-05 00:36:42 -0300

    feat: hash based routing for now

--- src/router.ts
+++ src/router.ts
@@ -4,30 +4,31 @@ import { loadRedeemPage } from "./pages/redeem";
 
 // URL Path based routing
 function handleRouting() {
-  console.log("Routing");
   const contentArea = document.getElementById("content-area");
 
   if (!contentArea) return;
 
-  switch (window.location.pathname) {
-    case "/mint":
+  switch (window.location.hash) {
+    case "#/mint":
       loadMintPage();
       break;
-    case "/redeem":
+    case "#/redeem":
       loadRedeemPage();
       break;
-    case "/":
-    case "/index.html":
+    case "":
+    case "#/":
+    case "#/index.html":
       loadHomePage();
       break;
     default:
-      // Redirect to home ("/") if no route matches
-      window.location.href = "/";
+      // Redirect to home if no route matches
+      window.location.hash = "#/";
       break;
   }
 }
 
-// Attach the routing function to DOMContentLoaded
+// Run handleRouting on hashchange and DOMContentLoaded
+window.addEventListener("hashchange", handleRouting);
 document.addEventListener("DOMContentLoaded", handleRouting);
 
 export {};
