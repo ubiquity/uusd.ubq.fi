77b8d988ff1b50e27ca837957b643d655f64932d
zugdev <zug.dev@proton.me>
2024-11-05 01:42:37 -0300

    feat: pull spot in home

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,11 +1,28 @@
-export function loadHomePage() {
+import { ethers } from "ethers";
+import { diamondContract } from "../main";
+
+export async function loadHomePage() {
   const contentArea = document.getElementById("content-area");
+
   if (contentArea) {
-    fetch("/home.html")
-      .then((response) => response.text())
-      .then((html) => {
-        contentArea.innerHTML = html;
-      })
-      .catch((error) => console.error("Error loading home page:", error));
+    try {
+      // load Home HTML
+      const response = await fetch("/home.html");
+      const html = await response.text();
+      contentArea.innerHTML = html;
+
+      // retrieve the SPOT price from Pool
+      const spotPriceRaw = await diamondContract.getDollarPriceUsd();
+      const spotPrice = ethers.utils.formatUnits(spotPriceRaw, 6);
+
+      // write price to page
+      const spotPriceElement = contentArea.querySelector("#DollarPrice p:first-of-type");
+
+      if (spotPriceElement) {
+        spotPriceElement.textContent = `$ ${spotPrice} (SPOT)`;
+      }
+    } catch (error) {
+      console.error("Error on home page:", error);
+    }
   }
 }
