809ec2e83c66c362c907e3f1eecc6782366ec31e
Kingsley Victor <javaprodigy56@gmail.com>
2024-11-08 23:03:59 +0100

    fix: knip issues

--- .github/knip.ts
+++ .github/knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["src/main.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "cypress/scripts/*.sh"],
+  ignore: ["src/types/config.ts", "src/scripts/custom-chains.ts", "**/__mocks__/**", "**/__fixtures__/**", "cypress/scripts/*.sh"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
   ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data", "@jest/globals"],
