7a02d386e5768547b0b5413943de0e92aadb4c60
Kingsley Victor <javaprodigy56@gmail.com>
2024-12-26 07:40:00 +0100

    chore: fix issues

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -2,8 +2,7 @@ export function truncateString(text: string) {
   return text.slice(0, 5) + "..." + text.slice(-5);
 }
 
-//eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function toSignificantFigures(i: any, sf: number = 3) {
+export function toSignificantFigures(i: number | string | bigint, sf: number = 3) {
   return Number(i).toLocaleString("en-US", {
     maximumFractionDigits: sf,
     useGrouping: true,
